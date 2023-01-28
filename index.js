515b302994d8c2b6c17648deb0810f40f1e1e41f
Kittikun Singkhamma <s.kittikun3@gmail.com>
2023-01-28 17:07:20 +0700

    28/1/23

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app.use(express.json());
 
 app.use(answerRoutes);
 
-app.listen(8888, () => {
-    console.log("Server is running on port :8888");
+app.listen(8080, () => {
+    console.log("Server is running on port :8080");
 });
